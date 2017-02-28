f91b20b2c64af39170b70c6914d9b9c3ee6df448
Scott <mehus.scott@gmail.com>
2017-02-28 08:25:27 -0500

    adding lava

---
+++ GravityWizard2/LavaSection.swift
@@ -0,0 +1,19 @@
+//
+//  LavaSection.swift
+//  GravityWizard2
+//
+//  Created by scott mehus on 2/28/17.
+//  Copyright © 2017 scott mehus. All rights reserved.
+//
+
+import SpriteKit
+
+final class LavaSection: SKSpriteNode {
+    
+}
+
+extension LavaSection: LifecycleListener {
+    func didMoveToScene() {
+        
+    }
+}
