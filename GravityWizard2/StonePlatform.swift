a86e5459bcc682e7a1276872deb68ff36ca3dfd9
Scott <mehus.scott@gmail.com>
2017-03-10 08:21:50 -0500

    Adding phyisics

---
+++ GravityWizard2/StonePlatform.swift
@@ -0,0 +1,25 @@
+//
+//  StonePlatform.swift
+//  GravityWizard2
+//
+//  Created by scott mehus on 3/10/17.
+//  Copyright © 2017 scott mehus. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+fileprivate struct Names {
+    static let tile = "tile"
+}
+
+final class StonePlatform: SKNode {
+    
+
+}
+
+extension StonePlatform: LifecycleListener {
+    func didMoveToScene() {
+        
+    }
+}
