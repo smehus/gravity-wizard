c1e0bc1e8da048d8f8f40da6c02d135987a303ba
Scott <mehus.scott@gmail.com>
2017-03-11 13:43:14 -0500

    Moving platform hell

--- GravityWizard2/StonePlatform.swift
+++ GravityWizard2/StonePlatform.swift
@@ -11,15 +11,29 @@ import SpriteKit
 
 fileprivate struct Names {
     static let tile = "tile"
+    static let container = "container"
 }
 
 final class StonePlatform: SKNode {
     
+    fileprivate func setupTileBodies() {
+        enumerateChildNodes(withName: "//\(Names.tile)") { node, _ in
+            guard let sprite = node as? SKSpriteNode else { return }
+            
+        }
+    }
 
+    fileprivate func setupBody(with sprite: SKSpriteNode) {
+        
+    }
+    
+    fileprivate func setupContainerBody(with node: SKNode) {
+        
+    }
 }
 
 extension StonePlatform: LifecycleListener {
     func didMoveToScene() {
-        
+        setupTileBodies()
     }
 }
