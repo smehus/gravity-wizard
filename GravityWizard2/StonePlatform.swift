7f47f4e3203f57cab6c86e199f968a953e72781f
Scott <mehus.scott@gmail.com>
2017-03-11 13:49:34 -0500

    Setting up bodies

--- GravityWizard2/StonePlatform.swift
+++ GravityWizard2/StonePlatform.swift
@@ -12,19 +12,35 @@ import SpriteKit
 fileprivate struct Names {
     static let tile = "tile"
     static let container = "container"
+    static let anchor = "StaticAnchor"
 }
 
 final class StonePlatform: SKNode {
     
+    fileprivate var anchor: SKNode?
+    
+    fileprivate func setupAnchorBody() {
+        guard let node = childNode(withName: Names.anchor) else {
+            assertionFailure("Failed to find anchor")
+            return
+        }
+        
+        anchor = node
+    }
+    
     fileprivate func setupTileBodies() {
-        enumerateChildNodes(withName: "//\(Names.tile)") { node, _ in
-            guard let sprite = node as? SKSpriteNode else { return }
+        enumerateChildNodes(withName: "//\(Names.tile)") { [weak self] node, _ in
+            guard
+                let sprite = node as? SKSpriteNode,
+                let text = sprite.texture
+            else {
+                assertionFailure("Failed to cast tile as sprite node")
+                return
+            }
+            
+            let body = SKPhysicsBody(rectangleOf: text.size())
             
         }
-    }
-
-    fileprivate func setupBody(with sprite: SKSpriteNode) {
-        
     }
     
     fileprivate func setupContainerBody(with node: SKNode) {
@@ -34,6 +50,7 @@ final class StonePlatform: SKNode {
 
 extension StonePlatform: LifecycleListener {
     func didMoveToScene() {
+        setupAnchorBody()
         setupTileBodies()
     }
 }
