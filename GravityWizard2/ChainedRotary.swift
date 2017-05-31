1c74cc306277b4b400b0d635a2f6cf3bb4fedf2a
Scott <mehus.scott@gmail.com>
2017-05-31 08:10:17 -0400

    Adding chained rotary

---
+++ GravityWizard2/ChainedRotary.swift
@@ -0,0 +1,25 @@
+//
+//  ChainedRotary.swift
+//  GravityWizard2
+//
+//  Created by scott mehus on 5/31/17.
+//  Copyright © 2017 scott mehus. All rights reserved.
+//
+
+import SpriteKit
+
+final class ChainedRotary: SKNode {
+    
+}
+
+extension ChainedRotary: LifecycleListener {
+    func didMoveToScene() {
+        
+    }
+}
+
+extension ChainedRotary: Obstacle {
+    func collision(at contactPoint: CGPoint) {
+        
+    }
+}
