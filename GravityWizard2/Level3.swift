51b9c29f7b4836a6094fcfaf70534dced24e2ac4
Scott <mehus.scott@gmail.com>
2017-06-08 08:36:28 -0400

    Level donzo

--- GravityWizard2/Level3.swift
+++ GravityWizard2/Level3.swift
@@ -69,7 +69,20 @@ final class Level3: GameScene {
     }
     
     override func levelCompleted() {
+        guard let successLevel = LevelCompleteLabel.createLabel(), let camera = camera else { return }
+        successLevel.move(toParent: camera)
+        successLevel.position = CGPoint.zero
         
+        
+        let presentScene = SKAction.afterDelay(2.0) {
+            guard let nextLevel = self.currentLevel.nextLevel()?.levelScene() else { return }
+            nextLevel.scaleMode = self.scaleMode
+            let transition = SKTransition.doorsOpenHorizontal(withDuration: 1.0)
+            self.view?.presentScene(nextLevel, transition: transition)
+            
+        }
+        
+        run(presentScene)
     }
     
     override func gameOver() {
