5e979de69e06762beeb290c23f9108c7bec86936
Scott <mehus.scott@gmail.com>
2017-07-05 18:29:20 -0400

    Adding water

---
+++ GravityWizard2/Sprites/Environment/WaterSceneNode.swift
@@ -0,0 +1,19 @@
+//
+//  WaterSceneNode.swift
+//  GravityWizard2
+//
+//  Created by scott mehus on 7/5/17.
+//  Copyright © 2017 scott mehus. All rights reserved.
+//
+
+import SpriteKit
+
+final class WaterSceneNode: SKNode {
+    
+}
+
+extension WaterSceneNode: LifecycleListener {
+    func didMoveToScene() {
+        
+    }
+}
