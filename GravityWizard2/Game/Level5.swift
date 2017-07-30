b28b7325bb8389ab916497a43d287788f4217d51
Scott <mehus.scott@gmail.com>
2017-07-30 17:48:45 -0400

    Moving file

--- GravityWizard2/Game/Level5.swift
+++ GravityWizard2/Game/Level5.swift
@@ -109,52 +109,6 @@ final class Level5: GameScene {
     }
 }
 
-enum Field {
-    case linear
-    case turbulence
-    case radial
-    case velocity
-    
-    func generate() -> SKFieldNode {
-        switch self {
-        case .linear:
-            return linearField()
-        case .radial:
-            return radialField()
-        case .turbulence:
-            return turbulenceField()
-        case .velocity:
-            return velocityField()
-        }
-    }
-    
-    private func turbulenceField() -> SKFieldNode {
-        let field = SKFieldNode.turbulenceField(withSmoothness: 1.0, animationSpeed: 5)
-        field.strength = 0.3
-        return field
-    }
-    
-    private func radialField() -> SKFieldNode {
-        let field = SKFieldNode.radialGravityField()
-        field.strength = 100.0
-        field.falloff = 1.0
-        field.isEnabled = true
-        return field
-    }
-    
-    private func linearField() -> SKFieldNode {
-        let field = SKFieldNode.linearGravityField(withVector: vector_float3(-1, 0, 0))
-        field.strength = 9.8
-        return field
-    }
-    
-    private func velocityField() -> SKFieldNode {
-        let field = SKFieldNode.velocityField(withVector: vector_float3(-1, 0, 0))
-        field.zPosition = 20
-        return field
-    }
-}
-
 // MARK: - End Level
 
 
