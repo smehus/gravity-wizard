79c71a06bb2e7902a578d12213ccc0393cef7778
Scott <mehus.scott@gmail.com>
2017-09-23 09:37:46 -0400

    menu stuff

--- GravityWizard2/Game/Menus/MainMenu.swift
+++ GravityWizard2/Game/Menus/MainMenu.swift
@@ -11,11 +11,13 @@ import SpriteKit
 private enum Sprite {
     case hero
     case startLabel
+    case startButton
     
     var name: String {
         switch self {
         case .hero: return "rose"
-        case .startLabel: return "start"
+        case .startLabel: return "startLabel"
+        case .startButton: return "startButton"
         }
     }
 }
@@ -23,7 +25,8 @@ private enum Sprite {
 class MainMenu: SKScene {
     
     private var rose: SKSpriteNode?
-    private var startButton: SKLabelNode?
+    private var startLabel: SKLabelNode?
+    private var startButton: SKSpriteNode?
     
     static func instantiate() -> MainMenu? {
         return SKScene(fileNamed: String(describing: MainMenu.self)) as? MainMenu
@@ -32,14 +35,16 @@ class MainMenu: SKScene {
     override func didMove(to view: SKView) {
         guard
             let roseNode = childNode(withName: Sprite.hero.name) as? SKSpriteNode,
-            let start = childNode(withName: Sprite.startLabel.name) as? SKLabelNode
+            let start = childNode(withName: Sprite.startLabel.name) as? SKLabelNode,
+            let button = childNode(withName: Sprite.startButton.name) as? SKSpriteNode
         else {
             conditionFailure(with: "failed to resolve sprites")
             return
         }
         
         rose = roseNode
-        startButton = start
+        startLabel = start
+        startButton = button
         
         // begin animating the character
     }
@@ -50,12 +55,11 @@ class MainMenu: SKScene {
         let position = touch.location(in: self)
         let touchNodes = nodes(at: position)
         
-        if let label = touchNodes.filter({ $0.name == Sprite.startLabel.name }).first as? SKLabelNode {
+        if let _ = touchNodes.filter({ $0.name == Sprite.startLabel.name || $0.name == Sprite.startButton.name }).first {
             // make label different color
-            label.fontColor = .red
+            startLabel?.fontColor = .red
         }
     }
-    
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
@@ -64,8 +68,12 @@ class MainMenu: SKScene {
         let touchNodes = nodes(at: position)
         // move forward to level picker
         
-        if let _ = touchNodes.filter({ $0.name == Sprite.startLabel.name }).first as? SKLabelNode {
+        if let _ = touchNodes.filter({ $0.name == Sprite.startButton.name || $0.name == Sprite.startButton.name }).first {
             // got to level selector
+            let nextMenu = LevelSelectorMenu.instantiate()
+            nextMenu.scaleMode = self.scaleMode
+            let transition = SKTransition.flipHorizontal(withDuration: 0.5)
+            view?.presentScene(nextMenu, transition: transition)
         }
     }
 }
